86f327b172d2d00cb5a8715ab12fe6621b6f3aff
danieluzoegbu <d.s.uzoegbu@gmail.com>
2023-03-26 07:43:28 +0100

    correct link path

--- components/Form/SignupForm.tsx
+++ components/Form/SignupForm.tsx
@@ -77,7 +77,7 @@ const SignupForm = () => {
       try {
         setLoading(true);
         const apiRes = await axios.post(
-          "http://localhost:3000/api/auth/signup",
+          "/api/auth/signup",
           data
         );
 
